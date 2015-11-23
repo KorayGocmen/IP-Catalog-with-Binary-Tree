371f3c9b61887f8439956ed91cf3e8935acc7532
Koray Gocmen <gocmen.koray@gmail.com>
2015-11-23 13:06:28 -0500

    Create TreeDB.h

---
+++ TreeDB.h
@@ -0,0 +1,61 @@
+#ifndef TREEDB_H
+#define	TREEDB_H
+
+#include <string>
+
+#include "DBentry.h"
+#include "TreeNode.h"
+
+using namespace std;
+
+class TreeDB{
+    
+private:
+   TreeNode* root;
+   int probesCount;
+   // You will need to add additional private functions
+
+public:
+   // the default constructor, creates an empty database.
+   TreeDB();
+
+   // the destructor, deletes all the entries in the database.
+   ~TreeDB();
+
+   // inserts the entry pointed to by newEntry into the database.
+   // If an entry with the same key as newEntry's exists
+   // in the database, it returns false. Otherwise, it returns true.
+   bool insert(DBentry* newEntry);
+
+   // searches the database for an entry with a key equal to name.
+   // If the entry is found, a pointer to it is returned.
+   // If the entry is not found, the NULL pointer is returned.
+   // Also sets probesCount
+   DBentry* find(string name);
+
+   // deletes the entry with the specified name (key)  from the database.
+   // If the entry was indeed in the database, it returns true.
+   // Returns false otherwise.
+   // See section 6 of the lab handout for the *required* removal method.
+   // If you do not use that removal method (replace deleted node by
+   // maximum node in the left subtree when the deleted node has two children)
+   // you will not match exercise's output.
+   bool remove(string name);
+    
+   // deletes all the entries in the database.
+   void clear();
+    
+   // prints the number of probes stored in probesCount
+   void printProbes(string name) const;
+   
+   // computes and prints out the total number of active entries
+   // in the database (i.e. entries with active==true).
+   void countActive () const;
+   
+   TreeNode* getRoot();
+   void setRoot(TreeNode* _root);
+   void print();
+
+};
+
+#endif	/* TREEDB_H */
