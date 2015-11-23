fc21170719942631302334b4d886778cfafed467
Koray Gocmen <gocmen.koray@gmail.com>
2015-11-23 13:03:31 -0500

    Create DBentry.cpp

---
+++ DBentry.cpp
@@ -0,0 +1,51 @@
+#include "DBentry.h"
+
+class DBentry;
+
+DBentry::DBentry(){
+	name="";
+	IPaddress=0;
+	active=false;
+}
+
+DBentry::DBentry(string _name, unsigned int _IPaddress, bool _active){
+	name = _name;
+	IPaddress = _IPaddress;
+	active = _active;    
+}
+
+DBentry::~DBentry(){}
+
+void DBentry::setName(string _name){
+	name = _name;
+}
+
+void DBentry::setIPaddress(unsigned int _IPaddress){
+	IPaddress = _IPaddress;
+}
+
+void DBentry::setActive(bool _active){
+	active = _active;
+}
+
+string DBentry::getName() const{
+	return name;
+}
+
+unsigned int DBentry::getIPaddress() const{
+	return IPaddress;
+}
+
+bool DBentry::getActive() const{
+	return active;
+}
+
+void DBentry::print(){
+	string activeValue;
+	if(this->active)
+    	activeValue = "active";
+	else
+    	activeValue = "inactive";
+    
+	cout << name << " : " << IPaddress << " : " << activeValue << endl;
+}
